c8c02ba1f4709bd64f86203c441678c54a3b652a
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-05-10 23:56:00 +0200

    adding milestone 3

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="card">
       <ul>
+          <li>
+              <img :src="`https://image.tmdb.org/t/p/w154/${info.poster_path}`" alt="poster">
+          </li>
           <li>Title: {{info.title == null ? info.name : info.title}}</li>
           <li>Original Title: {{info.original_title == null ? info.original_name : info.original_name}}</li>
          <li v-if="!flags.includes(info.original_language)">Original Language: {{info.original_language}}</li>
@@ -8,8 +11,9 @@
              <span>Original Language:</span>
              <img class="language" :src="require(`../assets/images/${info.original_language}.png`)" alt="language">
          </li>
-          <li>Vote: <i v-for="(n,i) in Math.ceil(info.vote_average /2)" :key="'element' + i" class="fas fa-star"></i>
+          <li>Vote: <i v-for="(n,i) in Math.ceil(info.vote_average /2)" :key="'element' + i" class="fas fa-star star"></i>
             <i v-for="(n,ind) in 5 - Math.ceil(info.vote_average /2)" :key="'el' + ind" class="far fa-star"></i></li>
+            <li>Overview: {{info.overview}}</li>
       </ul>
   </div>
 </template>
@@ -31,6 +35,10 @@ export default {
 .language {
     width: 20px;
     height: 10px;
+}
+
+.star {
+    color: yellow;
 }
 
 </style>
